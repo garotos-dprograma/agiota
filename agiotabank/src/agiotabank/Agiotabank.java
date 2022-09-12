63fb04ab82ff373d74f6ef670a0db5d60e9adaad
Rafael Lourenco <rafael.lourenco@sltlabs.com.br>
2022-09-12 19:43:48 -0300

    init projeto java

---
+++ agiotabank/src/agiotabank/Agiotabank.java
@@ -0,0 +1,12 @@
+package agiotabank;
+
+/**
+ * @author garotos de programa
+ */
+public class Agiotabank {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
